1d9171f0e83f6e96808fc6c18b326166280dd98f
Bharath <bharathkk68@gmail.com>
2022-08-12 09:22:42 -0400

    deploy

--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.path.join(BASE_DIR, 'secret.txt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-bloggers-app.heroku.com']
 
 
 # Application definition
